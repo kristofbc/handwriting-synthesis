99a6281a7ce4d947dfceff54f4dda153fd888823
Kristof Boucher Charbonneau <k.charbonneau@jp.honda-ri.com>
2017-09-15 17:23:24 +0900

    Separating data segmentation and execution

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -565,17 +565,18 @@ def main(data_dir, output_dir, batch_size, peephole, epochs, grad_clip, resume_d
 
         # Train the batch
         #optimizer.update(model, [train_data_batch, train_characters_batch, cs_data])
-        def train_model(i, model, b_size):
-            t_d_batch = train_data_batch[i*b_size:(i+1)*b_size]
-            cs_d_batch = cs_data[i*b_size:(i+1)*b_size]
+	def make_inputs_models(i, model, b_size):
+	    t_d_batch = train_data_batch[i*b_size:(i+1)*b_size]
+	    cs_d_batch = cs_data[i*b_size:(i+1)*b_size]
 
-            if model.xp != np:
-                t_d_batch = cuda.to_gpu(t_d_batch.copy(), gpu[i])
-                cs_d_batch = cuda.to_gpu(cs_d_batch.copy(), gpu[i])
-            
-            model([t_d_batch, cs_d_batch])
+	    if model.xp != np:
+		t_d_batch = cuda.to_gpu(t_d_batch.copy(), gpu[i])
+		cs_d_batch = cuda.to_gpu(cs_d_batch.copy(), gpu[i])
 
-        losses = op_models(models, train_model, batch_size/len(models))
+	    return [t_d_batch, cs_d_batch]
+
+	inputs_models = op_models(models, make_inputs_models, batch_size/len(models))
+	losses = op_models(models, lambda i, model, inputs: model(inputs[i]), inputs_models) 
 
         # Truncated back-prop at each time-step
         op_models(models, lambda i, model: model.cleargrads())
