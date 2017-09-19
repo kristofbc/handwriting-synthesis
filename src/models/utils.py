ba21ef3db88fd8bc2b3003123540761debee7cb4
Kristof Boucher Charbonneau <k.charbonneau@jp.honda-ri.com>
2017-09-19 12:30:09 +0900

    clip_grad kernel

--- src/models/utils.py
+++ src/models/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+from chainer import cuda
 
 def clip_grad(value, th_min, th_max, batch_size=None, xp=np):
     """
@@ -20,6 +21,22 @@ def clip_grad(value, th_min, th_max, batch_size=None, xp=np):
     # @NOTE: Prevent nan?
     #value = xp.where(xp.isnan(value), 1e-10, value)
     #value = xp.clip(value, th_min, th_max)
-    vmax = xp.max(xp.absolute(value), axis=1).reshape((batch_size, 1))
-    rate = xp.where(vmax > th_max, th_max/vmax, 1.0).astype(xp.float32).reshape((batch_size, 1))
-    return value * rate
+    res = xp.zeros_like(value)
+    if xp == np:
+        vmax = xp.max(xp.absolute(value), axis=1).reshape((batch_size, 1))
+        rate = xp.where(vmax > th_max, th_max/vmax, 1.0).astype(xp.float32).reshape((batch_size, 1))
+        res = value * rate
+    else:
+        # GPU clipping
+        cuda.elementwise(
+            'T value, T th_min, T th_max'
+            'T res',
+            '''
+                COMMON_ROUTINE;
+                vmax = fabsf(value)
+                rate = (vmax>th_max) ? th_max / vmax : 1.0
+                res = value * rate
+            ''',
+            'clip_grad_kern')(value, th_min, th_max, res)
+        
+    return res
