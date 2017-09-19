bc0066ae67750fb59dd50ef38c9113fc38c45776
Kristof Boucher Charbonneau <k.charbonneau@jp.honda-ri.com>
2017-09-19 14:02:07 +0900

    Fixing division by zero

--- src/models/utils.py
+++ src/models/utils.py
@@ -23,8 +23,11 @@ def clip_grad(value, th_min, th_max, batch_size=None, xp=np):
     #value = xp.clip(value, th_min, th_max)
     res = xp.zeros_like(value)
     if xp == np:
-        vmax = xp.max(xp.absolute(value), axis=1).reshape((batch_size, 1))
-        rate = xp.where(vmax > th_max, th_max/vmax, 1.0).astype(xp.float32).reshape((batch_size, 1))
+        #vmax = xp.max(xp.absolute(value), axis=1).reshape((batch_size, 1))
+        #rate = xp.where(vmax > th_max, th_max/vmax, 1.0).astype(xp.float32).reshape((batch_size, 1))
+        #res = value * rate
+        vmax = xp.absolute(value) + 1e-10
+        rate = xp.where(vmax > th_max, th_max/vmax, 1.0).astype(xp.float32).reshape(value.shape)
         res = value * rate
     else:
         # GPU clipping
@@ -33,7 +36,7 @@ def clip_grad(value, th_min, th_max, batch_size=None, xp=np):
             'T res',
             '''
                 COMMON_ROUTINE;
-                T vmax = fabsf(value);
+                T vmax = fabsf(value) + 1e-10;
                 T rate = (vmax>th_max) ? th_max / vmax : 1.0;
                 res = value * rate;
             ''',
