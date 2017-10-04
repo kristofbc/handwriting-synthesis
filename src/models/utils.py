f8fdb89d474b56ebbe69e4ca14feeb7ddec2b87f
Kristof Boucher Charbonneau <k.charbonneau@jp.honda-ri.com>
2017-10-04 16:54:14 +0900

    LinearLayerNormalizationLSTM

--- src/models/utils.py
+++ src/models/utils.py
@@ -18,6 +18,7 @@ def clip_grad(value, th_min, th_max, xp=np):
     #value = xp.where(xp.isnan(value), 1e-10, value)
     #value = xp.clip(value, th_min, th_max)
     res = xp.zeros_like(value)
+    return value
     if xp == np:
         #vmax = xp.max(xp.absolute(value), axis=1).reshape((batch_size, 1))
         #rate = xp.where(vmax > th_max, th_max/vmax, 1.0).astype(xp.float32).reshape((batch_size, 1))
@@ -41,3 +42,15 @@ def clip_grad(value, th_min, th_max, xp=np):
         #    raise ValueError("NaN detected")
         
     return res
+
+def mean_squared_error(true, pred):
+    """
+        Mean square error between the prediction and ground-truth
+        Args:
+            pred (float[][]): prediction
+            true (float[][]): ground-truth
+        Returns:
+            float|float[]
+    """
+    xp = cuda.get_array_module(*pred)
+    return xp.sum(xp.square(true - pred)) / pred.size
