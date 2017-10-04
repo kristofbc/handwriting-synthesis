28482b479f78184077dd5eb0446cb2e774de8a0e
Kristof Boucher Charbonneau <k.charbonneau@jp.honda-ri.com>
2017-10-04 17:44:04 +0900

    Fix parameters in LayerNorm

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -406,17 +406,27 @@ class LayerNormalization(chainer.Link):
     """
         Implementation of LayerNormalization from Ba, J. L., Kiros, J. R., & Hinton, G. E. (2016). Layer Normalization.
         Args:
-            out_size (int): hidden unit size
+            hidden_size (int|shape): shape of the hidden size
             bias_init (float): optional bias parameter
             gain_init (float): optional gain parameter
             epsilon (float): computation stability parameters
     """
-    def __init__(self, out_size, bias_init = 0., gain_init = 1., epsilon = 1e-5):
+    def __init__(self, hidden_size, bias_init = 0., gain_init = 1., epsilon = 1e-6):
         super(LayerNormalization, self).__init__()
-        self.out_size = out_size
-        self.bias = bias_init
-        self.gain = gain_init
+        self.hidden_size = hidden_size
         self.epsilon = epsilon
+
+        with self.init_scope():
+            self.bias = variable.Parameter(bias_init)
+            self.gain = variable.Parameter(gain_init)
+
+            if hidden_size is not None:
+                self._initialize_params(hidden_size)
+
+    def _initialize_params(self, size):
+        self.bias.initialize(size)
+        self.gain.initialize(size)
+        self.hidden_size = size
 
     def __call__(self, x):
         """
@@ -426,6 +436,9 @@ class LayerNormalization(chainer.Link):
             Returns:
                 float[][]
         """
+        if self.hidden_size is None:
+            self._initialize_params(x.shape)
+
         # Layer Normalization parameters
         mu = F.average(x, axis=1, keepdims=True)
         mu = F.broadcast_to(mu, x.shape)
@@ -460,7 +473,7 @@ class LinearLayerNormalizationLSTM(chainer.Chain):
 
         with self.init_scope():
             self.h_x = Linear(None, n_units)
-            self.norm_c = LayerNormalization(self.n_units, norm_bias_init, norm_gain_init)
+            self.norm_c = LayerNormalization(None, norm_bias_init, norm_gain_init)
 
     def reset_state(self):
         """
